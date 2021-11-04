a2baf008fb898a52c05ae07f5ab110bdf42dfab0
Eric <ericyoung111@gmail.com>
2021-11-04 14:29:28 -0600

    commiting my work

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const fs = require('fs')
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-    console.log('Portfolio complete! Check out index.html to see the output!')
+const profileDataArgs = process.argv.slice(2);
+
+console.log(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
 });
